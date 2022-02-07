5932a8e9e91b9abe606674530a17be490234f717
dcm1030 <dcm1030@wildcats.unh.edu>
2022-02-06 23:40:19 -0500

    july2021 and data cleaning
    - read 5 .csv files from july 2021 and clean them to be usable for RL code

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pandas as pd
+import read_csv
 
 T = 100
+GENERATOR_COST = -10
 
 class State:
 
-    def __init__(self, SoC, dem, pvgen, dem_pred, pvgen_pred):
+    def __init__(self, SoC, is_gen_on, dem, gen, dem_pred, gen_pred):
         self.battery_charge = SoC
+        self.is_generator_on = is_gen_on
         self.demand = dem
-        self.pv_gen = pvgen
+        self.generation = gen
         self.demand_prediction = dem_pred
-        self.pv_gen_prediction = pvgen_pred
+        self.gen_prediction = gen_pred
 
     # array = numpy.ndarray((T,),dtype=numpy.object)
     # for i in range(10):
@@ -19,8 +22,26 @@ class State:
     # for i in range(10):
     #     print array[i].method()
 
+
 def main():
-    states =
+    data_dict = read_csv.read_files()
+
+    for t in range(T):
+        pass
+
+
+def r(s: State, a):
+    # if generator is on or we are turning it on
+    if s.is_generator_on or (not s.is_generator_on and a == 1):
+        return GENERATOR_COST
+    else:
+        return 0
+
+
+def f(s: State, a):
+    pass
+
+
 
 
 if __name__ == '__main__':
--- read_csv.py
+++ read_csv.py
@@ -1,10 +1,68 @@
 import pandas as pd
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+'''
+read data from:
+ https://sustainablesml.org/pages/systemList.php
+ https://sustainablesml.org/pages/export.php
+
+data:
+ 2 ECB power generation csv files (wind/solar) -> 
+ 1 ECB battery soc file -> 
+ 1 Generator Room power demand csv file -> 
+ 1 diesel generator file -> map to indicator variable (on/off)??
+ 
+ how to create demand prediction?
+ how to create power generation prediction?
+'''
 
 
-def main():
-    pass
+def read_files():
+    mypath = 'data/july2021/'
+    # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    # for file in onlyfiles:
+
+    diesel_df = pd.read_csv(mypath + '2021-07-01_2021-07-31_diesel_generator_power.csv', comment='#').dropna()
+    demand_df = pd.read_csv(mypath + '2021-07-01_2021-07-31_islandPowerUse.csv', comment='#').dropna()
+    battery_df = pd.read_csv(mypath + '2021-07-01_2021-07-31_ecb_battery.csv', comment='#')[['Date', 'stateOfCharge (%)']].dropna()
+
+    pvpower_df = pd.read_csv(mypath + '2021-07-01_2021-07-31_ecb_solarpowergen.csv', comment='#').dropna()
+    windpower_df = pd.read_csv(mypath + '2021-07-01_2021-07-31_ecb_windpower.csv', comment='#').dropna()
+
+    # combine wind + power generation to one table
+    # only take rows on the 10 min intervals
+    powergen_df = pvpower_df.loc[pvpower_df['Date'].isin(battery_df['Date'])].reset_index(drop=True)
+    windpower_df = windpower_df.loc[windpower_df['Date'].isin(battery_df['Date'])].reset_index(drop=True)
+
+    # rename to match so we can add later
+    powergen_df.rename(columns={'pvHarvestPower (W)': 'Power (W)'}, inplace=True)
+    windpower_df.rename(columns={'power (kW)': 'Power (W)'}, inplace=True)
+
+    # convert to W
+    windpower_df['Power (kW)'] = (windpower_df['Power (W)'] * 1000)
+    # drop unneeded columns
+    windpower_df = windpower_df[['Date', 'Power (W)']]
+
+    # add solar and wind power together
+    powergen_df = pd.concat([powergen_df, windpower_df]).groupby('Date')['Power (W)'].sum().reset_index()
+
+    # battery % is only measured every 10min, only consider rows that line up with battery readings
+    demand_df = demand_df.loc[demand_df['Date'].isin(battery_df['Date'])].sort_values(by='Date').reset_index(drop=True)
+    diesel_df = diesel_df.loc[diesel_df['Date'].isin(battery_df['Date'])].sort_values(by='Date').reset_index(drop=True)
+    battery_df = battery_df.sort_values(by='Date').reset_index(drop=True)
+
+    print_df(powergen_df, 'powergen')
+    print_df(demand_df, 'demand')
+    print_df(diesel_df, 'diesel')
+    print_df(battery_df, 'battery')
+
+    return {'renewable': powergen_df,
+            'diesel': diesel_df,
+            'demand': demand_df,
+            'battery': battery_df}
 
 
-if __name__ == '__main__':
-    main()
-
+def print_df(df, name='df'):
+    print(f'{name}:\n {df}')
